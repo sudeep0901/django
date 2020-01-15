ca27733537b0bfeba6fe31c0e707a3fef5b8f6aa
Sudeep Patel <sudeep.tech.patel@gmail.com>
2020-01-15 15:55:50 +0530

    merged python web flask and other app

--- django-employee-app/hrapp/employee/models.py
+++ django-employee-app/hrapp/employee/models.py
@@ -22,9 +22,31 @@ class Employee(models.Model):
     employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
 
 
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+    def __str__(self):
+        return "%s the place" % self.name
 
 
+class Restaurant(models.Model):
+    place = models.OneToOneField(
+        Place,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s the restaurant" % self.place.name
 
 
+class Waiter(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
 
-
+    def __str__(self):
+        return "%s the waiter at %s" % (self.name, self.restaurant)
+    
